63785ddffc8358590d037f737a03733f4d77c682
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-11-07 16:15:12 +0530

    add

---
+++ src/app/pages/course/layout.js
@@ -0,0 +1,14 @@
+ 
+
+
+
+ export const metadata = {
+  title:  "HTML Course",
+  description: "learn Html and got placement in Google company many of our students got placement in FANG Companies",
+};
+
+const courseLayout = ({children}) => {
+  return <>{children}</>;
+};
+
+export default courseLayout;
